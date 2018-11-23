f31d98c5ed62bdc007862867309ebcfbfe880382
XShinHe <public@localhost.localdomain>
2018-11-23 14:18:29 +0800

    update

--- Correlation/Wolff/run_b.sh
+++ Correlation/Wolff/run_b.sh
@@ -3,17 +3,16 @@
 # this bash run a series Ising simulation in different tenperature
 # it covers b=0~1, where phase transition occurs around about b=0.44
 
+b0=0.01  # beta = 1/kT
+b=0.01
 J=1
 h=0
 echo H C M X > ${1}.csv
-for b in  0.44 \
-0.442 0.444 0.446 0.448 0.45 \
-0.452 0.454 0.456 0.458 0.46 \
-0.462 0.464 0.466 0.468 0.47 \
-0.472 0.474 0.476 0.478 0.48
+for(( i=1;i<=100;i++ ))
 do
-    #b=`echo "${b0} * $i"| bc -l`
+    b=`echo "${b0} * $i"| bc -l`
     ./ising.run ${b} ${J} ${h} >> ${1}.csv
-    echo ${b}
+    echo "$i / 100"
 done
 
+python3 plt2.py ${1}.csv ${b0}
